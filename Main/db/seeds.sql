44d89c898853a7abec1597061a05d07a705ac806
PacoCasillas <pacocasillasmx@gmail.com>
2023-05-24 21:51:21 -0400

    changed sql password to make it public, added seeds.sql info

--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -1,21 +1,21 @@
 USE employees_db;
 
--- Insert sample departments
+-- Sample departments
 INSERT INTO department (department_name) VALUES
   ('Sales'),
   ('Marketing'),
   ('Finance');
 
--- Insert sample roles
+-- Sample roles
 INSERT INTO role (title, salary, department_id) VALUES
   ('Manager', 50000, 1),
   ('Salesperson', 30000, 1),
   ('Marketing Coordinator', 35000, 2),
   ('Accountant', 45000, 3);
 
--- Insert sample employees
+-- Sample employees
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
   ('John', 'Doe', 1, NULL),
-  ('Jane', 'Smith', 2, 1),
-  ('Mike', 'Johnson', 3, 1),
-  ('Emily', 'Davis', 4, 3);
+  ('Austin', 'Powers', 2, 1),
+  ('Gold', 'Member', 3, 1),
+  ('Chico', 'Casillas', 4, 3);
